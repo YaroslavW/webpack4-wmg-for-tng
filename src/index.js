00ef39a7aeeeab15ddf15903782bb7714f8c885a
YaroslavW <doctor355@yahoo.com>
2019-03-19 12:23:00 +0300

    Some adding in index file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import {config} from './modules/config'
+import AppService from './modules/app.service'
+import './modules/header.component'
+
+
+console.log("Config key is", config.key);
+
+const service = new AppService('It is Hello from AppService');
+
+service.log();
